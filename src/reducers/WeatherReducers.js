0e7d27db78bdd9ce645422db0a49d2c5385d2f82
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-01-18 22:06:22 +0200

    minor changes

--- src/reducers/WeatherReducers.js
+++ src/reducers/WeatherReducers.js
@@ -6,7 +6,6 @@ export default function weatherReducer(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'SET_WEATHER_DATA':
             return {
-                ...state,
                 weatherData: action.weatherData
             }
         default:
